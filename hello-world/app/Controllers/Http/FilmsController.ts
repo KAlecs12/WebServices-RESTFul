2bd2c1e2b870066b93f5c3d0b31ae61d174a0663
bruno <alexandre.bruno@ynov.com>
2023-11-10 15:49:39 +0100

    commit

--- hello-world/app/Controllers/Http/FilmsController.ts
+++ hello-world/app/Controllers/Http/FilmsController.ts
@@ -3,7 +3,10 @@ import Film from "App/Models/Film";
 const xml2js = require('xml2js');
 
 export default class FilmsController {
-  public async generateResponse({ request, response, data }) {
+  /**
+   * Génère une réponse en JSON ou XML en fonction du header 'Accept'.
+   */
+  private async generateResponse({ request, response, data, status = 200 }) {
     const acceptHeader = request.header('Accept');
 
     if (acceptHeader && acceptHeader.includes('application/xml')) {
@@ -11,71 +14,73 @@ export default class FilmsController {
       const xml = builder.buildObject(data);
 
       response.header('Content-Type', 'application/xml');
+      response.status(status);
       return response.send(xml);
     } else {
       response.header('Content-Type', 'application/json');
+      response.status(status);
       return response.json(data);
     }
   }
 
-  public async index({ response }: HttpContextContract) {
-    const films = await Film.all();
-    return response.json(films);
-  }
-
-  // public async show({ params, response }: HttpContextContract) {
-  //   const film = await Film.find(params.id);
-  //   if (!film) {
-  //     return response.status(404).json({ message: 'Film not found' });
-  //   }
-  //   return response.json(film);
-  // }
-
-
-  public async findByName({ params, response }: HttpContextContract) {
+  public async findByName({ params, response, request }: HttpContextContract) {
     const film = await Film.findBy('name', params.name);
-    if (film === null) {
-      return response.status(404).json({ message: 'Film not found' });
+    if (!film) {
+      return this.generateResponse({
+        request,
+        response,
+        data: { message: 'Film not found' },
+        status: 404
+      });
     }
-    return response.json(film);
+    return this.generateResponse({ request, response, data: film });
   }
 
   public async create({ request, response }: HttpContextContract) {
-    // Extracting film data from the request body
     const filmData = request.only(['name', 'description', 'release_date', 'note']);
 
-    // Validating and handling note value
     if (filmData.note < 0 || filmData.note > 5) {
-      return response.status(400).json({ message: 'Note must be between 0 and 5' });
+      return this.generateResponse({
+        request,
+        response,
+        data: { message: 'Note must be between 0 and 5' },
+        status: 422
+      });
     }
 
-    // Creating a new film in the database
     const film = await Film.create(filmData);
-
-    // Returning the created film as a response
-    return response.status(201).json(film);
+    return this.generateResponse({ request, response, data: film, status: 201 });
   }
-
 
   public async update({ params, request, response }: HttpContextContract) {
     const film = await Film.find(params.id);
     if (!film) {
-      return response.status(404).json({ message: 'Film not found' });
+      return this.generateResponse({
+        request,
+        response,
+        data: { message: 'Film not found' },
+        status: 404
+      });
     }
 
     const filmData = request.only(['name', 'description', 'release_date', 'note']);
     film.merge(filmData);
     await film.save();
 
-    return response.json(film);
+    return this.generateResponse({ request, response, data: film });
   }
 
-  public async destroy({ params, response }: HttpContextContract) {
+  public async destroy({ params, request, response }: HttpContextContract) {
     const film = await Film.find(params.id);
     if (!film) {
-      return response.status(404).json({ message: 'Film not found' });
+      return this.generateResponse({
+        request,
+        response,
+        data: { message: 'Film not found' },
+        status: 404
+      });
     }
     await film.delete();
-    return response.status(204).json({ message: 'Film deleted' });
+    return this.generateResponse({ request, response, data: { message: 'Film deleted' }, status: 200 });
   }
 }
